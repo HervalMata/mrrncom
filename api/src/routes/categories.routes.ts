e0823b714e4f4ac7dcca5ac7696c699cdbbc257c
Herval Mata <hervalmataamparo@gmail.com>
2021-10-12 20:08:50 -0300

    feat: art: api - refactoring categories routes

--- api/src/routes/categories.routes.ts
+++ api/src/routes/categories.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
-import {createCategoryController} from "../modules/categories/controllers/createCategory";
-import { listCategoriesController } from "../modules/categories/controllers/listCategories";
+import createCategoryController from "../modules/categories/controllers/createCategory";
+import listCategoriesController from "../modules/categories/controllers/listCategories";
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
-    return createCategoryController.handle(req, res);
+    return createCategoryController().handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-    return listCategoriesController.handle(req, res);
+    return listCategoriesController().handle(req, res);
 });
 
 export { categoriesRoutes };
