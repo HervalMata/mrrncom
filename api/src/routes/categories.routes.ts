6a527673132fad8f9673df3c373cd13f83ddfb43
Herval Mata <hervalmataamparo@gmail.com>
2021-10-13 15:21:35 -0300

    feat: art: api - add find activate categories method in category routes

--- api/src/routes/categories.routes.ts
+++ api/src/routes/categories.routes.ts
@@ -3,16 +3,20 @@ import { Router } from "express";
 import { CreateCategoryController } from "../modules/categories/controllers/CreateCategoryController";
 import { ListCategoriesController} from "../modules/categories/controllers/ListCategoriesController";
 import {ActivateCategoriesController} from "../modules/categories/controllers/ActivateCategoriesController";
+import {ListActivateCategoriesController} from "../modules/categories/controllers/ListActivateCategoriesController";
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const activateCategoriesController = new ActivateCategoriesController();
+const listActivateCategoriesController = new ListActivateCategoriesController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.put("/activate", activateCategoriesController.handle);
+
+categoriesRoutes.get("/activate", listActivateCategoriesController.handle);
 
 export { categoriesRoutes };
