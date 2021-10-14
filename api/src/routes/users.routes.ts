d6f190e536b86a7ee5a5f01b490d9183b294866e
Herval Mata <hervalmataamparo@gmail.com>
2021-10-14 17:43:05 -0300

    feat: art: api - add ensure authenticated for avatar route

--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -3,14 +3,19 @@ import {CreateUserController} from "../modules/accounts/controllers/CreateUserCo
 import multer from "multer";
 import uploadConfig from '../config/upload';
 import {UpdateUserAvatarController} from "../modules/accounts/controllers/UpdateUserAvatarController";
+import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
-const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"));
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
-usersRoutes.patch("/avatar", uploadAvatar.single("avatar"), updateUserAvatarController.handle);
+usersRoutes.patch(
+    "/avatar", ensureAuthenticated,
+    uploadAvatar.single("avatar"),
+    updateUserAvatarController.handle
+);
 
 export { usersRoutes };
