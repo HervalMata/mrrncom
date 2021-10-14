07eb7a85171f3b268a9c66527fb6b658886b14b9
Herval Mata <hervalmataamparo@gmail.com>
2021-10-14 17:54:24 -0300

    feat: art: api - delet avatar

---
+++ api/src/utils/file.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+    try {
+        await fs.promises.stat(filename);
+    } catch (error) {
+        return;
+    }
+    await fs.promises.unlink(filename);
+}
