61dde092f07df3fd2b5456a2f1141e8090c8d323
Herval Mata <hervalmataamparo@gmail.com>
2021-10-14 14:51:58 -0300

    feat: art: api - create authenticate user service

---
+++ api/src/modules/accounts/controllers/AuthenticateUserController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express";
+import {container} from "tsyringe";
+import {AuthenticateUserService} from "../services/AuthenticateUserService";
+
+class AuthenticateUserController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const { email, password } = req.body;
+        const authenticateUserService = container.resolve(AuthenticateUserService);
+        const { user, token } = await authenticateUserService.execute({ email, password });
+        return res.status(201).json({ user, token });
+    }
+}
+
+export { AuthenticateUserController };
