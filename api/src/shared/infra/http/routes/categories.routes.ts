bba208e4aa5c1a0036f5b09973d89a7c2653fd28
Herval Mata <hervalmataamparo@gmail.com>
2021-10-14 21:27:03 -0300

    feat: art: api - create ensure admin middleware

--- api/src/shared/infra/http/routes/categories.routes.ts
+++ api/src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import { CreateCategoryController } from "../../../../modules/categories/control
 import { ListCategoriesController} from "../../../../modules/categories/controllers/ListCategoriesController";
 import {ActivateCategoriesController} from "../../../../modules/categories/controllers/ActivateCategoriesController";
 import {ListActivateCategoriesController} from "../../../../modules/categories/controllers/ListActivateCategoriesController";
+import {ensureAuthenticated} from "../../../../middlewares/ensureAuthenticated";
+import {ensureAdmin} from "../../../../middlewares/ensureAdmin";
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
@@ -11,11 +13,11 @@ const listCategoriesController = new ListCategoriesController();
 const activateCategoriesController = new ActivateCategoriesController();
 const listActivateCategoriesController = new ListActivateCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle);
 
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get("/", ensureAuthenticated, listCategoriesController.handle);
 
-categoriesRoutes.patch("/activate/:id", activateCategoriesController.handle);
+categoriesRoutes.patch("/activate/:id", ensureAuthenticated, ensureAdmin, activateCategoriesController.handle);
 
 categoriesRoutes.get("/activate", listActivateCategoriesController.handle);
 
