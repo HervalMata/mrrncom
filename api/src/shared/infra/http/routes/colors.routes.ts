3fe7f2efabc3b58e5d497fe955a413b1c459940e
Herval Mata <hervalmataamparo@gmail.com>
2021-10-15 14:11:23 -0300

    feat: art: api - create route for list colors

--- api/src/shared/infra/http/routes/colors.routes.ts
+++ api/src/shared/infra/http/routes/colors.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {CreateColorsController} from "../../../../modules/colors/controllers/CreateColorsController";
+import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
+import {ensureAdmin} from "../middlewares/ensureAdmin";
+import {ListColorsController} from "../../../../modules/colors/controllers/ListColorsController";
 
 const colorsRoutes = Router();
 const createColorsController = new CreateColorsController();
+const listColorsController = new ListColorsController();
 
-colorsRoutes.post("/", createColorsController.handle);
+colorsRoutes.post("/", ensureAuthenticated, ensureAdmin, createColorsController.handle);
+colorsRoutes.get("/", ensureAuthenticated, ensureAdmin, listColorsController.handle);
 
 export { colorsRoutes };
