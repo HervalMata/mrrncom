3ef9600275476bf0dc66849c839fc50721db7518
Herval Mata <hervalmataamparo@gmail.com>
2021-10-15 15:53:22 -0300

    feat: art: api - create route for list materials

--- api/src/shared/infra/http/routes/materials.routes.ts
+++ api/src/shared/infra/http/routes/materials.routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {CreateMaterialsController} from "../../../../modules/materials/controllers/CreateMaterialsController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
+import {ListMaterialsController} from "../../../../modules/materials/controllers/ListMaterialsController";
 
 const materialsRoutes = Router();
 const createMaterialsController = new CreateMaterialsController();
+const listMaterialsController = new ListMaterialsController();
 
 materialsRoutes.post("/", ensureAuthenticated, ensureAdmin, createMaterialsController.handle);
+materialsRoutes.get("/", ensureAuthenticated, ensureAdmin, listMaterialsController.handle);
 
 
 export { materialsRoutes };
