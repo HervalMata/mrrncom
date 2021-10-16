9d054af41c1106d9f2f99673a7cb967b09efa2aa
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 00:36:05 -0300

    feat: art: api - create route for list products

--- api/src/shared/infra/http/routes/products.routes.ts
+++ api/src/shared/infra/http/routes/products.routes.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import {CreateProductsController} from "../../../../modules/products/controllers/CreateProductsController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
+import {ListProductsController} from "../../../../modules/products/controllers/ListProductsController";
 
 const productsRoutes = Router();
 const createProductsController = new CreateProductsController();
+const listProductsController = new ListProductsController();
 
 productsRoutes.post("/", ensureAuthenticated, ensureAdmin, createProductsController.handle);
+productsRoutes.get("/", ensureAuthenticated, ensureAdmin, listProductsController.handle);
 
 export { productsRoutes };
