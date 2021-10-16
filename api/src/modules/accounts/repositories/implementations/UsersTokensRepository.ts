d55bbc52bb78d1e56588fa0abe38a07c3b46ff51
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 20:28:53 -0300

    feat: art: api - create structure of repository for users token

---
+++ api/src/modules/accounts/repositories/implementations/UsersTokensRepository.ts
@@ -0,0 +1,23 @@
+import {IUsersTokensRepository} from "../IUsersTokensRepository";
+import {ICreateUserTokenDTO} from "../../dtos/ICreateUserTokenDTO";
+import { UserTokens } from "../../entities/UserTokens";
+import {getRepository, Repository} from "typeorm";
+
+class UsersTokensRepository implements IUsersTokensRepository{
+    private repository: Repository<UserTokens>;
+
+    constructor() {
+        this.repository = getRepository(UserTokens);
+    }
+
+    async create({ id, refresh_token, expires_date }: ICreateUserTokenDTO): Promise<UserTokens> {
+        const userToken = this.repository.create({
+            id, refresh_token, expires_date
+        });
+        await this.repository.save(userToken);
+        return userToken;
+    }
+
+}
+
+export { UsersTokensRepository };
