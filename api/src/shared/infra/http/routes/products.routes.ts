29d24d4ab7b2d6c581fd1a7415d38711b6a0f313
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 15:09:05 -0300

    feat: art: api - create route for change price of products

--- api/src/shared/infra/http/routes/products.routes.ts
+++ api/src/shared/infra/http/routes/products.routes.ts
@@ -6,6 +6,7 @@ import {ListProductsController} from "../../../../modules/products/controllers/L
 import {ListAvailableProductsController} from "../../../../modules/products/controllers/ListAvailableProductsController";
 import {UpdateAvailabilityProductsController} from "../../../../modules/products/controllers/UpdateAvailabilityProductsController";
 import {UpdateStockProductsController} from "../../../../modules/products/controllers/UpdateStockProductsController";
+import {UpdatePriceProductsController} from "../../../../modules/products/controllers/UpdatePriceProductsController";
 
 const productsRoutes = Router();
 const createProductsController = new CreateProductsController();
@@ -13,6 +14,7 @@ const listProductsController = new ListProductsController();
 const listAvailableProductsController = new ListAvailableProductsController();
 const updateAvailabilityProductsController = new UpdateAvailabilityProductsController();
 const updateStockProductsController = new UpdateStockProductsController();
+const updatePriceProductsController = new UpdatePriceProductsController();
 
 productsRoutes.post("/", ensureAuthenticated, ensureAdmin, createProductsController.handle);
 productsRoutes.get("/", ensureAuthenticated, ensureAdmin, listProductsController.handle);
@@ -24,6 +26,7 @@ productsRoutes.patch("/available", ensureAuthenticated, ensureAdmin, updateAvail
 //Update Stock
 productsRoutes.patch("/stock", ensureAuthenticated, ensureAdmin, updateStockProductsController.handle);
 //Update Price
+productsRoutes.patch("/price", ensureAuthenticated, ensureAdmin, updatePriceProductsController.handle);
 //Update Offer
 //List Products By Category
 //Get Product Available
