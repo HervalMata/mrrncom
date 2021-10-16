00fa0ff774f6660a6407a03286a20ca559db84da
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 19:33:50 -0300

    feat: art: api - refactor server

---
+++ api/src/shared/infra/http/app.ts
@@ -0,0 +1,27 @@
+import express, {NextFunction} from 'express';
+import swaggerUi from "swagger-ui-express";
+import 'reflect-metadata'
+import '../typeorm';
+import '../../container';
+
+import "express-async-errors";
+import {HandlingErrors} from "./middlewares/HandlingErrors";
+import {createConnection} from "typeorm";
+
+import swaggerFile from '../../../swagger.json';
+import { router } from "./routes";
+
+createConnection();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+
+app.use(HandlingErrors);
+
+export { app };
--- api/src/shared/infra/http/server.ts
+++ api/src/shared/infra/http/server.ts
@@ -1,27 +1,3 @@
-import express, {NextFunction} from 'express';
-import swaggerUi from "swagger-ui-express";
-import 'reflect-metadata'
-import '../typeorm';
-import '../../container';
-
-import "express-async-errors";
-import {HandlingErrors} from "./middlewares/HandlingErrors";
-import {createConnection} from "typeorm";
-
-import swaggerFile from '../../../swagger.json';
-import { router } from "./routes";
-
-createConnection();
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-
-app.use(HandlingErrors);
+import { app } from "./app";
 
 app.listen(3333);
