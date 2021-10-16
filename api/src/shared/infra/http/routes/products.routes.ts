a493d3f31296864fcd9ced703cedd1597da4bb14
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 14:15:36 -0300

    feat: art: api - create route for change availability of products

--- api/src/shared/infra/http/routes/products.routes.ts
+++ api/src/shared/infra/http/routes/products.routes.ts
@@ -4,11 +4,13 @@ import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
 import {ListProductsController} from "../../../../modules/products/controllers/ListProductsController";
 import {ListAvailableProductsController} from "../../../../modules/products/controllers/ListAvailableProductsController";
+import {UpdateAvailabilityProductsController} from "../../../../modules/products/controllers/UpdateAvailabilityProductsController";
 
 const productsRoutes = Router();
 const createProductsController = new CreateProductsController();
 const listProductsController = new ListProductsController();
 const listAvailableProductsController = new ListAvailableProductsController();
+const updateAvailabilityProductsController = new UpdateAvailabilityProductsController();
 
 productsRoutes.post("/", ensureAuthenticated, ensureAdmin, createProductsController.handle);
 productsRoutes.get("/", ensureAuthenticated, ensureAdmin, listProductsController.handle);
@@ -16,6 +18,7 @@ productsRoutes.get("/available", listAvailableProductsController.handle);
 
 //ToDo
 //Update Available
+productsRoutes.patch("/available", ensureAuthenticated, ensureAdmin, updateAvailabilityProductsController.handle);
 //Update Stock
 //Update Price
 //Update Offer
