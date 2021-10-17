a311c560fe6a725d10141ac85eccdeb815808258
Herval Mata <hervalmataamparo@gmail.com>
2021-10-17 15:48:17 -0300

    feat: art: api - create route for reset users password

--- api/src/shared/infra/http/routes/password.routes.ts
+++ api/src/shared/infra/http/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import {SendForgotPasswordMailController} from "../../../../modules/accounts/controllers/SendForgotPasswordMailController";
+import {ResetUsersPasswordController} from "../../../../modules/accounts/controllers/ResetUsersPasswordController";
 
 const passwordRoutes = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetUsersPasswordController = new ResetUsersPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetUsersPasswordController.handle);
 
 export { passwordRoutes };
