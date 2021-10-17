2707e3e9fc9695778ff451d832452499ddd9410d
Herval Mata <hervalmataamparo@gmail.com>
2021-10-16 21:49:39 -0300

    feat: art: api - add refresh token route

--- api/src/shared/infra/http/routes/authenticate.routes.ts
+++ api/src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {AuthenticateUserController} from "../../../../modules/accounts/controllers/AuthenticateUserController";
+import {RefreshTokenController} from "../../../../modules/accounts/controllers/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes };
