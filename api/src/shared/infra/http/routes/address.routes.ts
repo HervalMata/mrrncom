3de344b8c8b0b07f677dcbc65807ff5a1964711d
Herval Mata <hervalmataamparo@gmail.com>
2021-10-18 20:09:00 -0300

    feat: art: api - create route for list users address

--- api/src/shared/infra/http/routes/address.routes.ts
+++ api/src/shared/infra/http/routes/address.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import {CreateAddressController} from "../../../../modules/address/controllers/CreateAddressController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
+import {ListAddressController} from "../../../../modules/address/controllers/ListAddressController";
+import {ensureAdmin} from "../middlewares/ensureAdmin";
 
 const addressRoutes = Router();
 const createAddressController = new CreateAddressController();
+const listAddressController = new ListAddressController();
 
 addressRoutes.post("/", ensureAuthenticated, createAddressController.handle);
+addressRoutes.get("/", ensureAuthenticated, ensureAdmin, listAddressController.handle);
 
 export { addressRoutes };
