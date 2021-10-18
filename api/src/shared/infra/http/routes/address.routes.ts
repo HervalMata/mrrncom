ebd0d1822895a2334ec3ddb4e852dbb4e40a6f36
Herval Mata <hervalmataamparo@gmail.com>
2021-10-18 20:36:32 -0300

    feat: art: api - create route for update users address

--- api/src/shared/infra/http/routes/address.routes.ts
+++ api/src/shared/infra/http/routes/address.routes.ts
@@ -4,14 +4,17 @@ import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ListAddressController} from "../../../../modules/address/controllers/ListAddressController";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
 import {GetAddressByUserController} from "../../../../modules/address/controllers/GetAddressByUserController";
+import {UpdateAddressController} from "../../../../modules/address/controllers/UpdateAddressController";
 
 const addressRoutes = Router();
 const createAddressController = new CreateAddressController();
 const listAddressController = new ListAddressController();
 const getAddressByUserController = new GetAddressByUserController();
+const updateAddressController = new UpdateAddressController();
 
 addressRoutes.post("/", ensureAuthenticated, createAddressController.handle);
 addressRoutes.get("/", ensureAuthenticated, ensureAdmin, listAddressController.handle);
 addressRoutes.get("/:user_id", ensureAuthenticated, getAddressByUserController.handle);
+addressRoutes.put("/:id", ensureAuthenticated, updateAddressController.handle);
 
 export { addressRoutes };
