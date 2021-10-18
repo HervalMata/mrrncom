22b260d692b7f41dcf894389b0a33f26e4ad9f41
Herval Mata <hervalmataamparo@gmail.com>
2021-10-17 22:41:12 -0300

    feat: art: api - create controller for list users profile

---
+++ api/src/modules/accounts/controllers/ListUsersProfileController.ts
@@ -0,0 +1,13 @@
+import {Request, Response} from "express";
+import {container} from "tsyringe";
+import {ListUsersProfileService} from "../services/ListUsersProfileService";
+
+class ListUsersProfileController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const listUsersProfileService = container.resolve(ListUsersProfileService);
+        const all = await listUsersProfileService.execute();
+        return res.json(all);
+    }
+}
+
+export { ListUsersProfileController };
