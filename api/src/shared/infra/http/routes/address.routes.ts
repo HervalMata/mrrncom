4cc8e47be7933f98fc3929431fcf6a9d835b1dde
Herval Mata <hervalmataamparo@gmail.com>
2021-10-18 20:18:16 -0300

    feat: art: api - create route for get users address

--- api/src/shared/infra/http/routes/address.routes.ts
+++ api/src/shared/infra/http/routes/address.routes.ts
@@ -3,12 +3,15 @@ import {CreateAddressController} from "../../../../modules/address/controllers/C
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ListAddressController} from "../../../../modules/address/controllers/ListAddressController";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
+import {GetAddressByUserController} from "../../../../modules/address/controllers/GetAddressByUserController";
 
 const addressRoutes = Router();
 const createAddressController = new CreateAddressController();
 const listAddressController = new ListAddressController();
+const getAddressByUserController = new GetAddressByUserController();
 
 addressRoutes.post("/", ensureAuthenticated, createAddressController.handle);
 addressRoutes.get("/", ensureAuthenticated, ensureAdmin, listAddressController.handle);
+addressRoutes.get("/:user_id", ensureAuthenticated, getAddressByUserController.handle);
 
 export { addressRoutes };
