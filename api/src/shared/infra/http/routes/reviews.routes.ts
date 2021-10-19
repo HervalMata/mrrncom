a76571edae3782ab97d7d7e32c09b93b0460b741
Herval Mata <hervalmataamparo@gmail.com>
2021-10-19 00:21:00 -0300

    feat: art: api - create route for list products reviews

--- api/src/shared/infra/http/routes/reviews.routes.ts
+++ api/src/shared/infra/http/routes/reviews.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import {CreateReviewController} from "../../../../modules/reviews/controllers/CreateReviewController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
+import {ListReviewsController} from "../../../../modules/reviews/controllers/ListReviewsController";
 
 
 const reviewsRoutes = Router();
 const createReviewController = new CreateReviewController();
+const listReviewsController = new ListReviewsController();
 
 reviewsRoutes.post("/", ensureAuthenticated, createReviewController.handle);
+reviewsRoutes.get("/", listReviewsController.handle);
 
 export { reviewsRoutes };
