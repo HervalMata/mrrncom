de5981f68d1789fe146e815d8debeb3da5419599
Herval Mata <hervalmataamparo@gmail.com>
2021-10-19 00:54:19 -0300

    feat: art: api - create route for get reviews by users

--- api/src/shared/infra/http/routes/reviews.routes.ts
+++ api/src/shared/infra/http/routes/reviews.routes.ts
@@ -3,15 +3,18 @@ import {CreateReviewController} from "../../../../modules/reviews/controllers/Cr
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ListReviewsController} from "../../../../modules/reviews/controllers/ListReviewsController";
 import {GetReviewsByProductController} from "../../../../modules/reviews/controllers/GetReviewsByProductController";
+import {GetReviewsByUserController} from "../../../../modules/reviews/controllers/GetReviewsByUserController";
 
 
 const reviewsRoutes = Router();
 const createReviewController = new CreateReviewController();
 const listReviewsController = new ListReviewsController();
 const getReviewsByProductController = new GetReviewsByProductController();
+const getReviewsByUserController = new GetReviewsByUserController();
 
 reviewsRoutes.post("/", ensureAuthenticated, createReviewController.handle);
 reviewsRoutes.get("/", listReviewsController.handle);
 reviewsRoutes.get("/:product_id", getReviewsByProductController.handle);
+reviewsRoutes.get("/:user_id", getReviewsByUserController.handle);
 
 export { reviewsRoutes };
