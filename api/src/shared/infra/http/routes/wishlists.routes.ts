7795df5778e83d356049ff927f42377170487cda
Herval Mata <hervalmataamparo@gmail.com>
2021-10-20 19:54:22 -0300

    feat: art: api - create route for get wishlist

--- api/src/shared/infra/http/routes/wishlists.routes.ts
+++ api/src/shared/infra/http/routes/wishlists.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {CreateWishlistController} from "../../../../modules/wishlists/controllers/CreateWishlistController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
+import {ListWishlistsController} from "../../../../modules/wishlists/controllers/ListWishlistsController";
 
 const wishlistsRoutes = Router();
 const createWishlistController = new CreateWishlistController();
+const listWishlistsController = new ListWishlistsController();
 
 wishlistsRoutes.post("/", ensureAuthenticated, createWishlistController.handle);
+wishlistsRoutes.get("/:user_id", ensureAuthenticated, listWishlistsController.handle);
 
 export { wishlistsRoutes };
