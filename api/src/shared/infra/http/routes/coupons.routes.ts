a60efef02c0df5fb2acd6bd61b06cd0cc5e7b9fb
Herval Mata <hervalmataamparo@gmail.com>
2021-10-21 19:40:47 -0300

    feat: art: api - create route for get coupons

--- api/src/shared/infra/http/routes/coupons.routes.ts
+++ api/src/shared/infra/http/routes/coupons.routes.ts
@@ -3,12 +3,15 @@ import {CreateCouponController} from "../../../../modules/coupons/controllers/Cr
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
 import {ListCouponsController} from "../../../../modules/coupons/controllers/ListCouponsController";
+import {GetCouponController} from "../../../../modules/coupons/controllers/GetCouponController";
 
 const couponsRoutes = Router();
 const createCouponController = new CreateCouponController();
 const listCouponsController = new ListCouponsController();
+const getCouponController = new GetCouponController();
 
 couponsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCouponController.handle);
 couponsRoutes.get("/", ensureAuthenticated, ensureAdmin, listCouponsController.handle);
+couponsRoutes.get("/:id", getCouponController.handle);
 
 export { couponsRoutes };
