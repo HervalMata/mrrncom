2ca3f874b2f70b2239830587e0e395847b176fa0
Herval Mata <hervalmataamparo@gmail.com>
2021-10-21 19:35:52 -0300

    feat: art: api - create controller for get coupons

---
+++ api/src/modules/coupons/controllers/GetCouponController.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from "express";
+import {container} from "tsyringe";
+import {GetCouponService} from "../services/GetCouponService";
+
+class GetCouponController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+        const getCouponService = container.resolve(GetCouponService);
+        await getCouponService .execute({
+            id: id as string,
+        });
+        return res.status(200).send();
+    }
+}
+
+export { GetCouponController };
