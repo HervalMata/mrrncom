29dc72f44aa4e66f71bb2c5bb9ec48fe1d3fe09a
Herval Mata <hervalmataamparo@gmail.com>
2021-10-22 22:44:52 -0300

    feat: art: api - create route for get by id deliveries

--- api/src/shared/infra/http/routes/deliveries.routes.ts
+++ api/src/shared/infra/http/routes/deliveries.routes.ts
@@ -3,12 +3,15 @@ import {CreateDeliveryController} from "../../../../modules/deliveries/controlle
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ListDeliveriesController} from "../../../../modules/deliveries/controllers/ListDeliveriesController";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
+import {GetByIdDeliveriesController} from "../../../../modules/deliveries/controllers/GetByIdDeliveriesController";
 
 const deliveriesRoutes = Router();
 const createDeliveryController = new CreateDeliveryController();
 const listDeliveriesController = new ListDeliveriesController();
+const getByIdDeliveriesController = new GetByIdDeliveriesController();
 
 deliveriesRoutes.post("/", ensureAuthenticated, createDeliveryController.handle);
 deliveriesRoutes.get("/", ensureAuthenticated, ensureAdmin, listDeliveriesController.handle);
+deliveriesRoutes.get("/:id", ensureAuthenticated, getByIdDeliveriesController.handle);
 
 export { deliveriesRoutes };
