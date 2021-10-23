b5742eb8a1e013b7a99dbb62fa83861bb533f934
Herval Mata <hervalmataamparo@gmail.com>
2021-10-22 23:18:28 -0300

    feat: art: api - create route for get by status deliveries

--- api/src/shared/infra/http/routes/deliveries.routes.ts
+++ api/src/shared/infra/http/routes/deliveries.routes.ts
@@ -5,16 +5,19 @@ import {ListDeliveriesController} from "../../../../modules/deliveries/controlle
 import {ensureAdmin} from "../middlewares/ensureAdmin";
 import {GetByIdDeliveriesController} from "../../../../modules/deliveries/controllers/GetByIdDeliveriesController";
 import {GetByTypeDeliveriesController} from "../../../../modules/deliveries/controllers/GetByTypeDeliveriesController";
+import {GetByStatusDeliveriesController} from "../../../../modules/deliveries/controllers/GetByStatusDeliveriesController";
 
 const deliveriesRoutes = Router();
 const createDeliveryController = new CreateDeliveryController();
 const listDeliveriesController = new ListDeliveriesController();
 const getByIdDeliveriesController = new GetByIdDeliveriesController();
 const getByTypeDeliveriesController = new GetByTypeDeliveriesController();
+const getByStatusDeliveriesController = new GetByStatusDeliveriesController();
 
 deliveriesRoutes.post("/", ensureAuthenticated, createDeliveryController.handle);
 deliveriesRoutes.get("/", ensureAuthenticated, ensureAdmin, listDeliveriesController.handle);
 deliveriesRoutes.get("/:id", ensureAuthenticated, getByIdDeliveriesController.handle);
 deliveriesRoutes.get("/:type", ensureAuthenticated, ensureAdmin, getByTypeDeliveriesController.handle);
+deliveriesRoutes.get("/:status", ensureAuthenticated, ensureAdmin, getByStatusDeliveriesController.handle);
 
 export { deliveriesRoutes };
