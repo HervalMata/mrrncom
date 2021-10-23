576b00bb3f2211a5087d265f376c1ad54d95b0d5
Herval Mata <hervalmataamparo@gmail.com>
2021-10-22 23:02:20 -0300

    feat: art: api - create route for get by type deliveries

--- api/src/shared/infra/http/routes/deliveries.routes.ts
+++ api/src/shared/infra/http/routes/deliveries.routes.ts
@@ -4,14 +4,17 @@ import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
 import {ListDeliveriesController} from "../../../../modules/deliveries/controllers/ListDeliveriesController";
 import {ensureAdmin} from "../middlewares/ensureAdmin";
 import {GetByIdDeliveriesController} from "../../../../modules/deliveries/controllers/GetByIdDeliveriesController";
+import {GetByTypeDeliveriesController} from "../../../../modules/deliveries/controllers/GetByTypeDeliveriesController";
 
 const deliveriesRoutes = Router();
 const createDeliveryController = new CreateDeliveryController();
 const listDeliveriesController = new ListDeliveriesController();
 const getByIdDeliveriesController = new GetByIdDeliveriesController();
+const getByTypeDeliveriesController = new GetByTypeDeliveriesController();
 
 deliveriesRoutes.post("/", ensureAuthenticated, createDeliveryController.handle);
 deliveriesRoutes.get("/", ensureAuthenticated, ensureAdmin, listDeliveriesController.handle);
 deliveriesRoutes.get("/:id", ensureAuthenticated, getByIdDeliveriesController.handle);
+deliveriesRoutes.get("/:type", ensureAuthenticated, ensureAdmin, getByTypeDeliveriesController.handle);
 
 export { deliveriesRoutes };
