a6166187d8444ad8e7429009e5c10a218ee1d743
Herval Mata <hervalmataamparo@gmail.com>
2021-10-22 22:33:59 -0300

    feat: art: api - create route for list deliveries

--- api/src/shared/infra/http/routes/deliveries.routes.ts
+++ api/src/shared/infra/http/routes/deliveries.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import {CreateDeliveryController} from "../../../../modules/deliveries/controllers/CreateDeliveryController";
 import {ensureAuthenticated} from "../middlewares/ensureAuthenticated";
+import {ListDeliveriesController} from "../../../../modules/deliveries/controllers/ListDeliveriesController";
+import {ensureAdmin} from "../middlewares/ensureAdmin";
 
 const deliveriesRoutes = Router();
 const createDeliveryController = new CreateDeliveryController();
+const listDeliveriesController = new ListDeliveriesController();
 
 deliveriesRoutes.post("/", ensureAuthenticated, createDeliveryController.handle);
+deliveriesRoutes.get("/", ensureAuthenticated, ensureAdmin, listDeliveriesController.handle);
 
 export { deliveriesRoutes };
